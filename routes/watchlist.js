ac9ae8c652b5e50266a7e412fbec9e5df278b7e3
Aleia <aleia@ms-MacBook.local>
2020-03-01 21:38:33 -0800

    watchlist route and http testing via 3rd party extension

--- routes/watchlist.js
+++ routes/watchlist.js
@@ -9,4 +9,35 @@ router.get('/', (req, res) => {
     console.log("Converted!")
 });
 
+// VIEW one meteor
+router.get('/:meteor', (req, res) => {
+    console.log("VIEW ROUTE")
+});
+
+// ADD one meteor to watchlist
+router.post('/:meteor/add', (req, res) => {
+    console.log("POST ROUTE")
+});
+
+
+router.patch('/:meteor/edit', (req, res) => {
+    console.log("PATCH ROUTE")
+});
+
+router.delete('/:meteor/delete', (req, res) => {
+    console.log("DELETE ROUTE")
+});
+
+// VIEW meteors in a location
+// LATER
+router.get('/:city', (req, res) => {
+    console.log("VIEW ROUTE")
+});
+
+// VIEW meteors in a location radius
+// LATER
+router.get('/:city/:radius', (req, res) => {
+    console.log("VIEW ROUTE")
+});
+
 module.exports = router
--- server.js
+++ server.js
@@ -17,54 +17,12 @@ db.once('open', () => console.log('Connected to database'))
 app.use(express.json())
 
 
-// data_json = JSON.stringify(r_file.data)
-
 const watchlistRouter = require('./routes/watchlist')
 app.use('/watchlist', watchlistRouter)
-
-// ROUTES
-// HOME - Gets all meteor data and displays watchlist
-// app.get('/', (req, res) => {
-//     res.send(r_file.data)
-//     console.log("Converted!")
-// });
 
 app.get('/', (req, res) => {
     res.send("Homepage")
 });
-
-
-// VIEW one meteor
-app.get('/:meteor', (req, res) => {
-    console.log("VIEW ROUTE")
-});
-
-// ADD one meteor to watchlist
-app.post('/:meteor/add', (req, res) => {
-    console.log("POST ROUTE")
-});
-
-
-app.patch('/:meteor/edit', (req, res) => {
-    console.log("PATCH ROUTE")
-});
-
-app.delete('/:meteor/delete', (req, res) => {
-    console.log("DELETE ROUTE")
-});
-
-// VIEW meteors in a location
-// LATER
-app.get('/:city', (req, res) => {
-    console.log("VIEW ROUTE")
-});
-
-// VIEW meteors in a location radius
-// LATER
-app.get('/:city/:radius', (req, res) => {
-    console.log("VIEW ROUTE")
-});
-
 
 
 
